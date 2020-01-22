f8c1f25bb0d962bce4cd6515c06c2628868b1707
VerityHill <verity.hill@ed.ac.uk>
2020-01-22 11:41:56 +0000

    finished with simulated fitting summary stats

--- Fitting_modules/topology_set.py
+++ Fitting_modules/topology_set.py
@@ -4,19 +4,35 @@ from collections import Counter
 def calculate_topology_params(coalescent_tree):
     
     
-    #colless
+    #colless and staircase measures
     colless = 0
-
+    sum_ratios = []
+    
     for nde in coalescent_tree.final_nodes:
 
         right = np.sum(i > nde.root_to_tip for i in coalescent_tree.sample_times)
         left = np.sum(i <= nde.root_to_tip for i in coalescent_tree.sample_times)
 
-
         difference = left - right
 
         colless += abs(difference)
         
+        if right != left:
+            uneven += 1
+
+        if left<right:
+            ratio = left/right
+        elif right<left:
+            ratio = right/left
+        else:
+            ratio = 1
+        
+    sum_ratios.append(ratio)
+        
+    staircase_1 = uneven/len(coalescent_tree.final_nodes)
+
+    staircase_2 = np.mean(sum_ratios)
+
     #At the moment, this includes the root in each step calculation  
     sackin = np.sum(coalescent_tree.total_steps)
     
@@ -50,4 +66,40 @@ def calculate_topology_params(coalescent_tree):
     delta_w = difference
     
     
+    ##max_ladder and IL_nodes
+    count_list = []
+
+    for nde in coalescent_tree.final_nodes:
+
+        ladder_count = 0
+
+        go_up_ladder(nde, ladder_count, count_list)
+        
+    longest_ladder = max(count_list)
+    
+    ladder_likeness = longest_ladder/len(coalescent_tree.tips)
+    
+    return colless, sackin, WD_ratio, delta_w, longest_ladder, ladder_likeness, staircase_1, staircase_2
+
+    
+    
+    
+    
+def go_up_ladder(nde, ladder_count, count_list):
+
+    
+    if (nde.new_children[0].type == "Ind" and nde.new_children[1].type == "Coal") or (nde.new_children[0].type == "Coal" and nde.new_children[1].type == "Ind"):
+        
+        ladder_count += 1
+        
+        for i in nde.new_children:
+            if i.type == "Coal":
+                go_up_ladder(i, ladder_count, count_list)
+                
+    else:
+        
+        count_list.append(ladder_count)
+        
+    return
+        
 
