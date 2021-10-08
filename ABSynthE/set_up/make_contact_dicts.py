51ceba9267b2662be29890721e0417741be57a36
ViralVerity <verity.hill@ed.ac.uk>
2021-10-08 12:30:25 +0100

    add ebola SLE contact information

--- ABSynthE/set_up/make_contact_dicts.py
+++ ABSynthE/set_up/make_contact_dicts.py
@@ -18,37 +18,30 @@ def make_contact_dicts(input_directory, config):
         agent_location[new_key] = data[key]
     config["agent_location"] = agent_location
 
-    with open(os.path.join(input_directory,"District_to_hh.txt")) as json_file:
-        config["dist_to_hh"] = json.load(json_file)
 
     with open(os.path.join(input_directory,"household_to_chiefdom.txt")) as json_file:
         config["hh_to_ch"] = json.load(json_file)
 
-    with open(os.path.join(input_directory,"Chiefdom_to_hh.txt")) as json_file:
-        config["ch_to_hh"] = json.load(json_file)
-
-    with open(os.path.join(input_directory,"Hh_to_people.txt")) as json_file:
+    with open(os.path.join(input_directory,"household_to_people.txt")) as json_file:
         config["hh_to_ppl"] = json.load(json_file)
+
+    with open(os.path.join(input_directory,"chiefdom_to_household.txt")) as json_file:
+        config["ch_to_hh"] = json.load(json_file)
 
     with open(os.path.join(input_directory,"chiefdom_to_people.txt")) as json_file:
         config["chiefdom_to_ppl"] = json.load(json_file)
 
-    with open(os.path.join(input_directory,"District_to_ppl.txt")) as json_file:
+    with open(os.path.join(input_directory,"district_to_household.txt")) as json_file:
+        config["dist_to_hh"] = json.load(json_file)
+
+    with open(os.path.join(input_directory,"district_to_people.txt")) as json_file:
         config["dist_to_ppl"] = json.load(json_file)
+    
+    with open(os.path.join(input_directory,"district_to_chiefdom.txt")) as json_file:
+        config["dist_to_ch"] = json.load(json_file)
 
     with open(os.path.join(input_directory,"district_relative_distance.txt")) as json_file:
         config["district_distance"] = json.load(json_file)
-        
-    with open(os.path.join(input_directory,"dist_to_ch.txt")) as json_file:
-        config["dist_to_ch"] = json.load(json_file)
-
-  
-    #Do we actually use this one? Drawn from census directly anyway
-    with open(os.path.join(input_directory,"district_population.csv", 'r')) as f:
-        for l in f:
-            toks = l.strip("\n").split(",")
-            district_pops[toks[0]] = int(toks[1])
-    config["district_pops"] = district_pops
             
             
     return config
