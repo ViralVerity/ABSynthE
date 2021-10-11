c8208be8f5bc55a29a16c75ac1d58f06c9030888
ViralVerity <verity.hill@ed.ac.uk>
2021-10-11 17:25:30 +0100

    change contact behav for WAR and WAU to just pick someone else in the whole district

--- absynthe/classes/case_class.py
+++ absynthe/classes/case_class.py
@@ -1,5 +1,5 @@
-from absynthe.classes.individual_class import *
 import sys
+import random
 
 class Case():
     
@@ -13,11 +13,14 @@ class Case():
     def get_options_district(self, option_dict_district_level, config, parent_individual):
 
         if parent_individual.ch in option_dict_district_level.keys():
-            dist_ppl_list = option_dict_districtlevel[parent_individual.ch]
-
+            dist_ppl_list = option_dict_district_level[parent_individual.ch]
+        elif parent_individual.dist == "westernarearural" or parent_individual.dist == "westernareaurban":
+            #not sure if this is the right behaviour
+            dist_ppl_list = random.choice([config["population_structure"]["hh_to_ppl"][hh] for hh in config["population_structure"]["ch_to_hh"][parent_individual.ch] 
+                                                if hh != parent_individual.hh]))
         else:
-            dist_ppl_list = ([config["cluster_to_ppl"]["population_structure"][chief] for chief in config["cluster_to_ppl"]["population_structure"]["dist_to_ch"][parent_individual.dist] if chief != parent_individual.ch])
-            option_dict_district_level[parent_individual.comm] = dist_ppl_list
+            dist_ppl_list = ([config["population_structure"]["ch_to_ppl"][chief] for chief in config["population_structure"]["dist_to_ch"][parent_individual.dist] if chief != parent_individual.ch])
+            option_dict_district_level[parent_individual.ch] = dist_ppl_list
 
         return dist_ppl_list, option_dict_district_level
 
@@ -27,45 +30,40 @@ class Case():
         Finds out which of the parent's potential contacts are still susceptible"""
                 
         if len(epidemic_config["infected_individuals_set"]) == config["population_structure"]["popn_size"]: 
-            return 0
+            return False
 
-        if self.level == "Hh":
+        if self.level == "Hh": #within household
             poss_case = random.choice([person for person in config["population_structure"]["hh_to_ppl"][parent_individual.hh] if person != parent_individual.unique_id]) 
 
-        elif self.level == "Ch":
+        elif self.level == "Ch": #within chiefdom, between household
             poss_case = random.choice(random.choice([config["population_structure"]["hh_to_ppl"][hh] for hh in config["population_structure"]["ch_to_hh"][parent_individual.ch] 
                                                 if hh != parent_individual.hh]))
-        elif self.level == "Dist":
-            ##don't remember why I did this, so commenting out for now
-            # if parent_individual.dist == "westernarearural" or parent_individual.dist == "westernareaurban":
-            #     return #why would I would to not assign someone if their parent was in WAR/WAU?
-            # else:
-            dist_ppl_list, option_dict_district_level = self.get_options_district(epidemic_config["option_dict_district_level"], config, parent_individual)
-            poss_case = random.choice(random.choice(dist_ppl_list))
-            epidemic_config['option_dict_district_level'] - option_dict_district_level
+        
+        elif self.level == "Dist": #within district, between chiefdom
+                #NB if person in WAR and WAU, they're currently drawn from anywhere in that district for this level
+                dist_ppl_list, option_dict_district_level = self.get_options_district(epidemic_config["option_dict_district_level"], config, parent_individual)
+                poss_case = random.choice(random.choice(dist_ppl_list))
+                epidemic_config['option_dict_district_level'] = option_dict_district_level
 
         elif self.level == "Country":        
             if day == 0:
                 district = "kenema"
             else:
                 district = random.choice(config["population_structure"]["district_distance"][parent_individual.dist])
-            poss_case = random.choice(dist_to_ppl[district])
+            poss_case = random.choice(config["population_structure"]["dist_to_ppl"][district])
 
         else:
             sys.stderr.write("ERROR no level assigned")
 
         #Is the person actually susceptible
         if poss_case not in epidemic_config["infected_individuals_set"]:
-            new_individual = Individual(poss_case, parent_individual, config["population_structure"]["agent_location"], config["cfr"], config["distributions"], day, epidemic_config)
-            epidemic_config["case_dict"][self] = new_individual
+            return poss_case, config, epidemic_config
             
         elif day == 0: #So that there are actually 14 cases in the first transmission cluster
             self.who_am_I(parent_individual, day, config, epidemic_config)
 
         else:
-            return False
-
-        return poss_case, config, epidemic_config
+            return 0
     
     
     
