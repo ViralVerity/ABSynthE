8af12d4c6f63f5f8335a1d977cecf265ebd6457b
ViralVerity <verity.hill@ed.ac.uk>
2021-10-11 20:10:49 +0100

    remove extra contact dicts

--- absynthe/set_up/make_contact_dicts.py
+++ absynthe/set_up/make_contact_dicts.py
@@ -2,7 +2,7 @@ import json
 from collections import defaultdict
 import os
 
-#Outputs are the same except that the cluster is now a chiefdom
+##add the other function from index funcs in here
 
 def make_contact_dicts(input_directory, config):
     
@@ -18,21 +18,14 @@ def make_contact_dicts(input_directory, config):
         agent_location[new_key] = data[key]
     config["population_structure"]["agent_location"] = agent_location
 
-
-    # with open(os.path.join(input_directory,"household_to_chiefdom.txt")) as json_file:
-    #     config["hh_to_ch"] = json.load(json_file)
-
     with open(os.path.join(input_directory,"household_to_people.txt")) as json_file:
         config["population_structure"]["hh_to_ppl"] = json.load(json_file)
 
     with open(os.path.join(input_directory,"chiefdom_to_household.txt")) as json_file:
         config["population_structure"]["ch_to_hh"] = json.load(json_file)
 
-    # with open(os.path.join(input_directory,"chiefdom_to_people.txt")) as json_file:
-    #     config["ch_to_ppl"] = json.load(json_file)
-
-    # with open(os.path.join(input_directory,"district_to_household.txt")) as json_file:
-    #     config["dist_to_hh"] = json.load(json_file)
+    with open(os.path.join(input_directory,"chiefdom_to_people.txt")) as json_file:
+        config["population_structure"]["ch_to_ppl"] = json.load(json_file)
 
     with open(os.path.join(input_directory,"district_to_people.txt")) as json_file:
         config["population_structure"]["dist_to_ppl"] = json.load(json_file)
