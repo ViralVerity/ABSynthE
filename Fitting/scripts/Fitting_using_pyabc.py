735d39ce54ec66c63df2584bb4f95ce1b5db5746
ViralVerity <verity.hill@ed.ac.uk>
2021-12-16 14:34:53 +0000

    fix typo

--- Fitting/scripts/Fitting_using_pyabc.py
+++ Fitting/scripts/Fitting_using_pyabc.py
@@ -37,10 +37,10 @@ def main(sysargs = sys.argv[1:]):
         summary_stats = list(observed_SS[0][0])
         function = simulate_pyabc_bl
     elif summary_stats_set == "topology":
-        summmary_stats = list(observed_SS[0][1])
+        summary_stats = list(observed_SS[0][1])
         function = simulate_pyabc_top
     elif summary_stats_set == "ltt":
-        summmary_stats = list(observed_SS[0][2])
+        summary_stats = list(observed_SS[0][2])
         function = simulate_pyabc_ltt
     elif summary_stats_set == "ltt_points":
         summary_stats = list(observed_SS[0][3])
