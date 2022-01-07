08e5ef609cc7ee64ddf9326b0d3c29da790c1c0d
ViralVerity <verity.hill@ed.ac.uk>
2022-01-07 10:41:22 +0000

    add error message to find out where the mean of 0 or similar is happening

--- Fitting/scripts/branch_length_parameters.py
+++ Fitting/scripts/branch_length_parameters.py
@@ -1,6 +1,7 @@
 #take whole tree as input
 
 import numpy as np
+import datetime as dt
 
 def calculate_branch_statistics(coalescent_tree):
     
@@ -41,4 +42,24 @@ def calculate_branch_statistics(coalescent_tree):
     
     branch_stats = [mean_lengths, median_lengths, var_lengths, mean_external, median_external, var_external, mean_internal, median_internal, var_internal, mean_ratio, median_ratio, var_ratio]
     
+
+    if len(coalescent_tree.b_len_list) == 0 or len(coalescent_tree.external_branches) == 0 or len(coalescent_tree.internal_branches) == 0:
+        ind_count = 0
+        for i in coalescent_tree.all_tips_nodes:
+            if i.type == 'individual':
+                ind_count += 1
+        now = dt.datetime.now()
+        
+        with open(f"weird_run_{now}.txt", 'w') as fw:
+            fw.write(f'{differences}\n')
+            fw.write(f'len coalescent nodes: {len(coalescent_tree.final_nodes)}\n')
+            fw.write(f'number of tips: {ind_count}\n')
+            fw.write(f'total in all_tips_nodes: {len(coalescent_tree.all_tips_nodes)}\n')
+            fw.write(f'all branches: {coalescent_tree.b_len_list}\n')
+            fw.write(f'external branches: {coalescent_tree.external_branches}\n')
+            fw.write(f'internal branches: {coalescent_tree.internal_branches}\n')
+
+        with open(f"test_newick_{now}.txt", 'w') as fw:
+            fw.write(newick_string)
+
     return branch_stats
