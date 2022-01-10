8fda78bb13376b66f45a5e9e17bfaf6402fda9c5
ViralVerity <verity.hill@ed.ac.uk>
2022-01-10 11:21:33 +0000

    add more topo tests

--- Fitting/scripts/topology_set.py
+++ Fitting/scripts/topology_set.py
@@ -48,7 +48,9 @@ def calculate_topology_params(coalescent_tree, newick_string):
             
             if left_count == 0 and right_count == 0:
                 print("zero on left and right count")
-                print(node_to_all_children)
+                # print(node_to_all_children)
+                print(left, right)
+                print(direct_children)
                 print(len(coalescent_tree.all_tips_nodes))
             if left_count < right_count:
                 ratio = left_count/right_count
